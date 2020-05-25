d8aaa81fe539cf0e3bb27ab89799b2b198e115c0
Willem Melis <willem.melis@outlook.com>
2020-05-25 18:31:55 +0200

    setup proj to do nmpc example on

---
+++ demos/NMPC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using kul.forbes;
+using kul.forbes.panocsharp;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Rosenbrock
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Demo of PANOC API");
+
+            //var degree = 2;
+            //var costFunction = new VectorFunction(x => (x.PointwisePower(degree).Sum(), degree * x.PointwisePower(degree - 1)));
+            //var constraint = new ProxBox(size: 1, penalty: 1, dimension: 2);
+            //var defaultConfig = new ConfigPanoc(problemDimension: 2);
+
+            //var solver = new PANOCSolver(costFunction,constraint,defaultConfig);
+            //var solution = solver.Solve(new double[] { 1.0,1.0 });
+
+            //Console.WriteLine($"Solved with solution: [{solution[0]};{solution[1]}]");
+        }
+    }
+}
